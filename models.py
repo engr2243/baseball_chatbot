e0ecd44f536e8d0eb4861f59a616553a91ca9742
engr2243 <engr2243@gmail.com>
2023-03-14 13:03:46 +0500

    new file:   app.py 	new file:   forms.py 	new file:   important.txt 	new file:   models.py 	new file:   static/chatbot_style.css 	new file:   static/images/LOGO.jpg 	new file:   static/images/background.png 	new file:   static/images/jasson-dominguez-new-york-yankees.png 	new file:   templates/auth.html 	new file:   templates/base.html 	new file:   templates/index.html

---
+++ models.py
@@ -0,0 +1,11 @@
+from app_creation import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<User %r>' % self.email
