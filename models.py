fad29525a6eaf2fffb0e657dea4da920170ca455
engr2243 <engr2243@gmail.com>
2023-03-14 12:51:44 +0500

    final version-0

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     
     def __repr__(self):
